0c35e97b5b7600a683bc1660d81879de2007f26f
cq0122 <cq0122@163.com>
2020-08-26 18:03:20 +0800

    theme

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <ul style={{ textAlign: "left" }}>
+          <li>
+            less修改变量：
+            <a
+              className="App-link"
+              href="./less-modifyvars/index.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              less modifyvars
+            </a>
+          </li>
+          <li>
+            加载不同css文件：
+            <a
+              className="App-link"
+              href="./append-link/index.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              append link
+            </a>
+          </li>
+          <li>
+            修改className：
+            <a
+              className="App-link"
+              href="./modify-className"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              modify className
+            </a>
+          </li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
