0872a031be300792c05441d0c29e5655eadd111d
cq0122 <cq0122@163.com>
2020-09-14 15:39:38 +0800

    theme

--- src/index.js
+++ src/index.js
@@ -8,7 +8,12 @@ import * as serviceWorker from "./serviceWorker";
 
 import Iconfont from "iconfontcn";
 
+// Iconfont.config("i", "t-", "testfont");
+
 Iconfont.init("font_2043983_w1m0lhxtor.js", "t-", "testfont");
+
+// Iconfont.init("font_2043983_w1m0lhxtor.css", "t-", "testfont");
+
 // Iconfont.init("//at.alicdn.com/t/font_2043983_w1m0lhxtor.css", "t-", "testfont");
 
 ReactDOM.render(
